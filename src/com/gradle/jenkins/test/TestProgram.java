4a1af8e3dd47d47aacf35dd83aa496389c72411f
Aniket Banerjee -X (anikbane - CYBAGE SOFTWARE INC at Cisco) <anikbane@partnet.cisco.com>
2016-03-03 19:22:08 +0530

    Removing Tab Spaces

--- src/com/gradle/jenkins/test/TestProgram.java
+++ src/com/gradle/jenkins/test/TestProgram.java
@@ -7,28 +7,22 @@ import java.io.IOException;
 
 public class TestProgram {
 
-	public static void main(String[] args) {
-		try {
-			String content = "This is the content to write into file.\n This is the first change in the file.";
-
-			File file = new File("GradleJenkinsTest.txt");
-
-			// if file doesnt exists, then create it
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(content);
-			bw.close();
-
-			System.out.println("Done");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
+  public static void main(String[] args) {
+    try {
+      String content = "This is the content to write into file.\n This is the first change in the file.";
+      File file = new File("GradleJenkinsTest.txt");
+      // if file doesnt exists, then create it
+      if (!file.exists()) {
+        file.createNewFile();
+      }
+      FileWriter fw = new FileWriter(file.getAbsoluteFile());
+      BufferedWriter bw = new BufferedWriter(fw);
+      bw.write(content);
+      bw.close();
+      System.out.println("Done");
+   } catch (IOException e) {
+     // TODO Auto-generated catch block
+     e.printStackTrace();
+   }
+ }
 }
